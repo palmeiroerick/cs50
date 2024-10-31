d4c6faf64a4bc6f31c0062dd84239a3c0c716438
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-10-31 16:45:40 -0300

    mario more

---
+++ Week 01/mario/more/mario.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height = 0;
+
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    for (int line = 1; line <= height; line++)
+    {
+        for (int space = height - line; space >= 1; space--)
+        {
+            printf(" ");
+        }
+        for (int hash = line; hash >= 1; hash--)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int hash = line; hash >= 1; hash--)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
