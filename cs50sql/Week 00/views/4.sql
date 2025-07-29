f4fa5c15613ea71ea2f038c2d229e00c84e9e370
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2025-07-29 17:44:27 -0300

    36 views

---
+++ cs50sql/Week 00/views/4.sql
@@ -0,0 +1,1 @@
+select count(*) from "views" where "artist" = 'Hiroshige' and "english_title" like '%Eastern Capital%';
---
+++ cs50sql/Week 00/views/8.sql
@@ -0,0 +1,1 @@
+select "english_title" from "views" where "artist" = 'Hokusai' order by "contrast" limit 5;
